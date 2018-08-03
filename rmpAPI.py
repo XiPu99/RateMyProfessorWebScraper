a29d272200c313a4a25be9fd0e5d81a20081ea55
Xi Pu <xp19@duke.edu>
2018-08-03 16:47:26 -0700

    Add comments

--- rmpAPI.py
+++ rmpAPI.py
@@ -1,5 +1,6 @@
 import requests, sys
 
+# check if users have entered more than one command line arguments
 if len(sys.argv) > 1:
     name = ' '.join(sys.argv[1:])
     url = 'http://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=20&q={0}+AND+schoolid_s%3A1350&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq='.format(name)
@@ -19,6 +20,7 @@ if len(sys.argv) > 1:
     if numFound == 0:
         print('No results found.')
 
+    # exactly one result is found
     elif numFound == 1:
         id = responseObj['docs'][0]['pk_id']
         pageNum = 1
@@ -29,24 +31,30 @@ if len(sys.argv) > 1:
         data = r.json()
         ratings = data['ratings']
 
+        # keep printing all comments until there is no more comment
         while data['remaining'] >= 0 and len(ratings)>0:
             for rating in ratings:
                 print(rating['rComments'])
                 print()
 
+            # update page number which is part of url below
             pageNum += 1
+            # form the url by using page number and teacher's id
             url = 'http://www.ratemyprofessors.com/paginate/professors/ratings?tid={0}&page={1}&max=20&cache=false'.format(id, pageNum)
             r = requests.get(url)
             r.raise_for_status()
             data = r.json()
             ratings = data['ratings']
 
+    # more than one result are found
     else:
         print(str(numFound) + ' results found:')
-
         results = responseObj['docs']
+
+        # print all search results
         for prof in results:
             print(prof['teacherfirstname_t'] + ' ' + prof['teacherlastname_t'])
 
+# no command line argument is entered
 else:
     print('Please enter something in the command line argument')
